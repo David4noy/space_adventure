3ba06483af13877c74ab4df58fef59c2c75db87a
David Noy <david4noy@gmail.com>
2025-03-21 17:02:21 +0200

    adding the game, a player and a joystick

---
+++ lib/components/player.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'package:flame/components.dart';
+
+class Player extends SpriteComponent with HasGameReference {
+  @override
+  Future<void> onLoad() async {
+
+    sprite = await game.loadSprite('player_blue_on0.png');
+
+    size *= 0.3;
+
+    return super.onLoad();
+  }
+}
--- lib/game_main.dart
+++ lib/game_main.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
+import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:space_adventure/components/player.dart';
 
 class GameMain extends FlameGame {
+
+  late Player player;
+  late JoystickComponent joystick;
 
   @override
   Future<void> onLoad() async {
@@ -10,6 +15,37 @@ class GameMain extends FlameGame {
     await Flame.device.fullScreen();
     await Flame.device.setPortrait();
 
+    startGame();
+
     return super.onLoad();
+  }
+
+  void startGame() async {
+    await _createJoystick();
+    _createPlayer();
+  }
+
+  void _createPlayer() {
+    player = Player()
+    ..anchor = Anchor.center
+    ..position = Vector2(size.x / 2, size.y * 0.8);
+
+    add(player);
+  }
+
+  Future<void> _createJoystick() async {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: await loadSprite('joystick_knob.png'),
+        size: Vector2.all(50),
+      ),
+      background: SpriteComponent(
+        sprite: await loadSprite('joystick_background.png'),
+        size: Vector2.all(100),
+      ),
+      anchor: Anchor.bottomLeft,
+      position: Vector2(20, size.y - 20)
+    );
+    add(joystick);
   }
 }
